8b1315e10566fcf070a04f57b7f628bfc75d5d55
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-27 00:02:26 -0300

    Delete exibirDuracaoEmHoras.kt
    Obsoleto. Função movida para dentro da classe

--- src/exercicio3Classes/exibirDuracaoEmHoras.kt
+++
@@ -1,7 +0,0 @@
-package exercicio3Classes
-
-fun exibirDuracaoEmHoras(titulo: String, duracaoEmMinutos: Int) {
-    val duracaoHoras = duracaoEmMinutos / 60
-    val minutosRestantes = duracaoEmMinutos % 60
-    println("O filme $titulo possui $duracaoHoras horas e $minutosRestantes minutos.")
-}
