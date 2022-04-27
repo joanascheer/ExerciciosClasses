b84166d8cdddaf0c13a0a990f6266b7052520712
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-27 00:00:41 -0300

    Update Filme.kt
    Refatorando, criando mais funções e as colocando dentro do escopo da classe Filmes

--- src/exercicio3Classes/Filme.kt
+++ src/exercicio3Classes/Filme.kt
@@ -1,8 +1,45 @@
-package exercicio3Classes
+package exercicio3Filmes
 
-class Filme {
-    var titulo = ""
-    var duracaoEmMinutos = 0
+class Filme (
+    tituloFilmeParametro: String,
+    duracaoEmMinutosParametro: Int
+) {
+    var tituloFilme = tituloFilmeParametro
+    var duracaoEmMinutos = duracaoEmMinutosParametro
 
+    val listaDeFilmes: ArrayList <Filme> = ArrayList()
+
+    fun exibirDuracaoEmHoras() {
+        var duracaoEmHoras = duracaoEmMinutos / 60
+        var restoMinutos = duracaoEmMinutos % 60
+
+        println("O filme $tituloFilme tem $duracaoEmHoras horas e $restoMinutos minutos.")
+    }
+
+    fun criaFilme(): Filme {
+
+        println("Qual o nome do filme?")
+        tituloFilme = readln()
+
+        println("Qual a sua duração em minutos?")
+        duracaoEmMinutos = readln().toInt()
+
+        return Filme(
+            tituloFilmeParametro = tituloFilme,
+            duracaoEmMinutosParametro = duracaoEmMinutos
+        )
+    }
+
+    fun criaListaFilmes(filme: Filme) {
+        listaDeFilmes.add(filme)
+    }
+
+
+    fun mostraFilmesEmCartaz() {
+        println("*** Em cartaz: ***\n")
+
+        listaDeFilmes.forEach {
+            it.exibirDuracaoEmHoras()
+        }
+    }
 }
-
