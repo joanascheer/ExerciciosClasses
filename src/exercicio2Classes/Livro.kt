05696a76bf16d704779e45501275ab9410aa4918
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-26 23:26:28 -0300

    Refatorado
    Colocando funções dentro do escopo da classe Livro e separando melhor as funções por responsabilidade.

--- src/exercicio2Classes/Livro.kt
+++ src/exercicio2Classes/Livro.kt
@@ -1,11 +1,52 @@
-package exercicio2Classes
+package exercicio2Livros
 
-class Livro {
-    var titulo = ""
-    var qtdPaginas = 0
-    var paginasLidas = 0
+class Livro (
+    tituloParametro: String,
+    qtdePaginasParametro: Int,
+    paginasLidasParametro: Int
+        ) {
+    var titulo = tituloParametro
+    var qtdePaginas = qtdePaginasParametro
+    var paginasLidas = paginasLidasParametro
 
+    val listaLivros: ArrayList<Livro> = ArrayList()
+
+    fun verificarProgresso() : Int {
+        return (paginasLidas * 100) / qtdePaginas
+    }
+
+    fun exibeMensagem() {
+        println("$titulo tem $qtdePaginas páginas.")
+        println("Você já leu ${verificarProgresso()}% do livro.")
+    }
+
+    fun criarLivro(): Livro {
+
+        println("Qual o título do livro?")
+        val tituloLivro = readln()
+
+        println("Quantas páginas tem este livro?")
+        val qtdePaginasLivro = readln().toInt()
+
+        println("Quantas páginas você já leu?")
+        val paginasLidasLivro = readln().toInt()
+
+        return Livro(
+            tituloParametro = tituloLivro,
+            qtdePaginasParametro = qtdePaginasLivro,
+            paginasLidasParametro = paginasLidasLivro
+        )
+    }
+
+    fun inserirLivroLista(livro: Livro) {
+        listaLivros.add(livro)
+    }
+
+    fun exibeListaLivros() {
+        println("*** Estante de livros ***\n")
+
+        listaLivros.forEach {
+            it.exibeMensagem()
+        }
+    }
 }
-
-
-
