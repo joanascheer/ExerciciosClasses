e5cab6c28061d06ed610c50b4fea56ebc6c43b1b
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-26 23:27:11 -0300

    Delete verificarProcesso.kt
    Obsoleto. Passado para dentro da classe

--- src/exercicio2Classes/verificarProcesso.kt
+++
@@ -1,6 +0,0 @@
-package exercicio2Classes
-
-fun verificarProcesso(qtdPaginas: Int, paginasLidas: Int) {
-    val porcentagem = paginasLidas * 100 / qtdPaginas
-    println("$porcentagem% do livro foi lido.")
-}
